58ede464031e450f42a701b7f2cc09a576a37d55
NicholasJYee <njsyee@gmail.com>
2025-05-22 17:08:27 -0400

    Working connections outside wsl + diff devices

--- chatbot_arena/run.py
+++ chatbot_arena/run.py
@@ -15,7 +15,9 @@ sys.path.insert(0, current_dir)
 from app import app
 
 if __name__ == '__main__':
-    port = 5000
-
     # Run Flask app on all interfaces
-    app.run(debug=True, host='127.0.0.1', port=port) 
+    print(f"\nRun it on other devices on your network with: http://192.168.1.39:5000/")
+    print(f"Run this to forward wsl-2 to windows (make sure port 5000 is open):")
+    print(f"netsh interface portproxy add v4tov4 listenaddress=0.0.0.0 listenport=5000 connectaddress=172.29.27.105 connectport=5000")
+    print(f"Source: https://ersantana.com/devops/exposing-flask-app-on-wsl-to-local-network\n\n")
+    app.run(host='0.0.0.0', port=5000) 
